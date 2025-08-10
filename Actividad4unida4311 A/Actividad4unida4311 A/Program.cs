edc3b10bc985623c91b310927fccb8a1fd84f571
Andry22A <andridelarosa2020@gmail.com>
2025-08-10 00:26:13 -0400

    Actividad4unida4311-A

---
+++ Actividad4unida4311 A/Actividad4unida4311 A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Actividad4unida4311_A
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmPrincipal());  // Cambia aquí al nombre real de tu formulario menú principal
+        }
+    }
+}
